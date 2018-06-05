aa974d7f6c406bf474316acb4fc50827d83c535b
ueeieiie <udielenberg@gmail.com>
2018-06-05 23:58:57 +0300

    ADD installed new package - webrtc-adapter run install next time you pull!

--- client/src/index.js
+++ client/src/index.js
@@ -2,12 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "mobx-react";
 import { injectGlobal } from "styled-components";
-
+import "webrtc-adapter";
 import { stores } from "./stores/";
 import App from "Components/App";
 
 const appLocation = document.querySelector(".app");
-
 
 ReactDOM.render(
 	<Provider {...stores}>
